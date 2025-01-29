dde047a3d21bc256259cbafb270eca129502969b
Tawana-doodles <matogotawananyasha@gmail.com>
2025-01-29 14:15:24 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import plotly a pt
 
 # Set page title
 st.set_page_config(page_title="Researcher Profile and STEM Data Explorer", layout="wide")
